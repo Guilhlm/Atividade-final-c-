c437b436433f52b8620e5b15f4dbd015ce685bc7
Guilherme Maia <141831938+Guilhlm@users.noreply.github.com>
2024-02-13 21:25:07 -0300

    agnda_telefonica

---
+++ agenda_telefonica/agenda_telefonica/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace agenda_telefonica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_SALVAR_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TXT_NOME.Text) || string.IsNullOrEmpty(TXT_TELEFONE.Text))
+            {
+                return;
+            }
+            else
+            {
+                ListViewItem escrita = new ListViewItem(TXT_NOME.Text);
+
+                escrita.SubItems.Add(TXT_TELEFONE.Text);
+
+                listView.Items.Add(escrita);
+
+                TXT_NOME.Clear();
+                TXT_TELEFONE.Clear();
+                TXT_NOME.Focus();
+            }         
+        }
+
+        private void BTN_REMOVER_Click(object sender, EventArgs e)
+        {
+
+            if (listView.Items.Count > 0)
+            {
+                listView.Items.Remove(listView.SelectedItems[0]);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
